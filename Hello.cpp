792ebf14b9fb02109df03ca6a15f8631e18b8fff
RingMaster777 <rashikjoy777@gmail.com>
2024-10-07 08:43:19 +0600

    initial

---
+++ Hello.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+#define ll long long
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    cout << "Hello from Home PC";
+
+    return 0;
+}
